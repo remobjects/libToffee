f4b8590d1b77f51292e33af847810c8d892c4ec3
marc hoffman <mh@remobjects.com>
2015-10-19 10:13:41 +0200

    73412: Implement char.isEqual on boxedchar/ansichar

--- Source/Boxing.pas
+++ Source/Boxing.pas
@@ -19,7 +19,10 @@ type
     class method boxedStructWithValue(aValue: ^Void; aDtor: __ElementsBoxedStructDestructor): __ElementsBoxedStruct;
     class method valueForStruct(aStruct: NSObject; aDtor: __ElementsBoxedStructDestructor): ^Void;
     property Dtor: __ElementsBoxedStructDestructor read fDtor;
-    property Value: ^Void read fValue;    
+    property Value: ^Void read fValue;
+    { INSObject }
+    method isEqual(object: id): Boolean; override;
+    method isEqualTo(object: id): Boolean; override;
     { INSCopying }
     method copyWithZone(zone: ^NSZone): not nullable id;
     { INSCoding }
@@ -28,13 +31,23 @@ type
   end;
 
   __ElementsBoxedChar = public class(NSObject, INSCopying, INSCoding)
+  private
+    method stringValue: NSString;
   public
     constructor withChar(aChar: Char);
     class method boxedCharWithChar(aChar: Char): __ElementsBoxedChar;
     method description: String; override;
     property charValue: Char; readonly;
+    property stringValue: NSString read stringValue;
     operator Implicit(aValue: __ElementsBoxedChar): NSNumber;
     operator Implicit(aValue: __ElementsBoxedChar): Char;
+    { INSObject }
+    method isEqual(object: id): Boolean; override;
+    method isEqualTo(object: id): Boolean; override;
+    method isGreaterThan(object: id): Boolean; override;
+    method isGreaterThanOrEqualTo(object: id): Boolean; override;
+    method isLessThan(object: id): Boolean; override;
+    method isLessThanOrEqualTo(object: id): Boolean; override;
     { INSCopying }
     method copyWithZone(zone: ^NSZone): not nullable id;
     { INSCoding }
@@ -43,13 +56,23 @@ type
   end;
 
   __ElementsBoxedAnsiChar = public class(NSObject, INSCopying, INSCoding)
+  private
+    method stringValue: NSString;
   public
     constructor withAnsiChar(aAnsiChar: AnsiChar);
     class method boxedAnsiCharWithAnsiChar(aAnsiChar: AnsiChar): __ElementsBoxedAnsiChar;
     method description: String; override;
     property ansiCharValue: AnsiChar; readonly;
+    property stringValue: NSString read stringValue;
     operator Implicit(aValue: __ElementsBoxedAnsiChar): NSNumber;
     operator Implicit(aValue: __ElementsBoxedAnsiChar): AnsiChar;
+    { INSObject }
+    method isEqual(object: id): Boolean; override;
+    method isEqualTo(object: id): Boolean; override;
+    method isGreaterThan(object: id): Boolean; override;
+    method isGreaterThanOrEqualTo(object: id): Boolean; override;
+    method isLessThan(object: id): Boolean; override;
+    method isLessThanOrEqualTo(object: id): Boolean; override;
     { INSCopying }
     method copyWithZone(zone: ^NSZone): not nullable id;
     { INSCoding }
@@ -84,6 +107,52 @@ end;
 class method __ElementsBoxedChar.boxedCharWithChar(aChar: Char): __ElementsBoxedChar;
 begin
   exit new __ElementsBoxedChar withChar(aChar);
+end;
+
+method __ElementsBoxedChar.stringValue: NSString;
+begin
+  var char := charValue;
+  result := NSString.stringWithCharacters(@char) length(1);
+end;
+
+method __ElementsBoxedChar.isEqual(object: id): Boolean;
+begin
+  result := isEqualTo(object)
+end;
+
+method __ElementsBoxedChar.isEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (charValue = (object as __ElementsBoxedChar).charValue)) or
+            ((object is __ElementsBoxedAnsiChar) and (charValue = Char((object as __ElementsBoxedAnsiChar).ansiCharValue))) or
+            ((object is NSString) and (stringValue = (object as NSString)));
+end;
+
+method __ElementsBoxedChar.isGreaterThan(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (charValue > (object as __ElementsBoxedChar).charValue)) or
+            ((object is __ElementsBoxedAnsiChar) and (charValue > (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is NSString) and (stringValue > (object as NSString)));
+end;
+
+method __ElementsBoxedChar.isGreaterThanOrEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (charValue >= (object as __ElementsBoxedChar).charValue)) or
+            ((object is __ElementsBoxedAnsiChar) and (charValue >= (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is NSString) and (stringValue >= (object as NSString)));
+end;
+
+method __ElementsBoxedChar.isLessThan(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (charValue < (object as __ElementsBoxedChar).charValue)) or
+            ((object is __ElementsBoxedAnsiChar) and (charValue < (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is NSString) and (stringValue < (object as NSString)));
+end;
+
+method __ElementsBoxedChar.isLessThanOrEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (charValue <= (object as __ElementsBoxedChar).charValue)) or
+            ((object is __ElementsBoxedAnsiChar) and (charValue <= (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is NSString) and (stringValue <= (object as NSString)));
 end;
 
 method __ElementsBoxedChar.copyWithZone(zone: ^NSZone): not nullable id;
@@ -124,6 +193,52 @@ end;
 operator __ElementsBoxedAnsiChar.Implicit(aValue: __ElementsBoxedAnsiChar): AnsiChar;
 begin
   result := aValue.ansiCharValue;
+end;
+
+method __ElementsBoxedAnsiChar.stringValue: NSString;
+begin
+  var char := Char(ansiCharValue);
+  result := NSString.stringWithCharacters(@char) length(1);
+end;
+
+method __ElementsBoxedAnsiChar.isEqual(object: id): Boolean;
+begin
+  result := isEqualTo(object)
+end;
+
+method __ElementsBoxedAnsiChar.isEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedAnsiChar) and (ansiCharValue = (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is __ElementsBoxedChar) and (Char(ansiCharValue) = (object as __ElementsBoxedChar).charValue)) or
+            ((object is NSString) and (stringValue = (object as NSString)));
+end;
+
+method __ElementsBoxedAnsiChar.isGreaterThan(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (ansiCharValue > (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is __ElementsBoxedChar) and (Char(ansiCharValue) > (object as __ElementsBoxedChar).charValue)) or
+            ((object is NSString) and (stringValue > (object as NSString)));
+end;
+
+method __ElementsBoxedAnsiChar.isGreaterThanOrEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (ansiCharValue >= (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is __ElementsBoxedChar) and (Char(ansiCharValue) >= (object as __ElementsBoxedChar).charValue)) or
+            ((object is NSString) and (stringValue >= (object as NSString)));
+end;
+
+method __ElementsBoxedAnsiChar.isLessThan(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (ansiCharValue < (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is __ElementsBoxedChar) and (Char(ansiCharValue) < (object as __ElementsBoxedChar).charValue)) or
+            ((object is NSString) and (stringValue < (object as NSString)));
+end;
+
+method __ElementsBoxedAnsiChar.isLessThanOrEqualTo(object: id): Boolean;
+begin
+  result := ((object is __ElementsBoxedChar) and (ansiCharValue <= (object as __ElementsBoxedAnsiChar).ansiCharValue)) or
+            ((object is __ElementsBoxedChar) and (Char(ansiCharValue) <= (object as __ElementsBoxedChar).charValue)) or
+            ((object is NSString) and (stringValue <= (object as NSString)));
 end;
 
 class method __ElementsBoxedAnsiChar.boxedAnsiCharWithAnsiChar(aAnsiChar: AnsiChar): __ElementsBoxedAnsiChar;
@@ -180,6 +295,17 @@ begin
   if lSelf.Dtor <> aDtor then
     raise new NSException withName('InvalidCastException') reason ('Destructor does not match for boxed struct') userInfo(nil);
   exit lSelf.fValue;
+end;
+
+{ INSObject }
+method __ElementsBoxedStruct.isEqual(object: id): Boolean;
+begin
+  result := false;
+end;
+
+method __ElementsBoxedStruct.isEqualTo(object: id): Boolean;
+begin
+  result := false;
 end;
 
 method __ElementsBoxedStruct.copyWithZone(zone: ^NSZone): not nullable id;
