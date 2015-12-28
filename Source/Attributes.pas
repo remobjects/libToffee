cceffe4b84b7b654f6786fd774173ef1c426a5ca
Carlo Kok <ck@remobjects.com>
2015-12-28 14:54:37 +0100

    71165: Nougat attributes

--- Source/Attributes.pas
+++ Source/Attributes.pas
@@ -1,10 +1,13 @@
 ﻿namespace RemObjects.Elements.System;
 
 interface
+uses
+  Foundation;
 
 type
-  Attribute = public class(Object)
+  __ElementsAttribute = public class(Object)
   end;
+  Attribute = public __ElementsAttribute;
   
   AttributeTargets = public flags (
     &Class = 1 shl 2,
@@ -16,17 +19,164 @@ type
     &Interface = 1 shl 10,
     &Parameter = 1 shl 11,
     &ReturnValue = 1 shl 13);
-  AttributeUsageAttribute = public class(Attribute)
+  __ElementsAttributeUsageAttribute = public class(Attribute)
   public
     constructor (aValidOn: AttributeTargets); 
     property ValidOn: AttributeTargets; readonly;
   end;
+  AttributeUsageAttribute = public __ElementsAttributeUsageAttribute;
+
+method __ElementsReadAttributes(var aAttributesPtr: ^Byte; aTargetParameter: Integer := 0; aTypeFilter: &Class): NSArray<Attribute>; public;
+method __ElementsReadAttribute(aAttributesPtr, aEnd: ^Byte; aTargetParameter: Integer; aTypeFilter: &Class): Attribute; public;
+method __ElementsGetAttributesBlockForFunction(aMethod: ^Void): ^Byte; public;
+method __ElementsGetAttributesBlockForMethod(aClass: &Class; aInstance: Boolean; aSel: SEL): ^Byte; public;
   
 implementation
 
 constructor AttributeUsageAttribute(aValidOn: AttributeTargets);
 begin
   ValidOn := aValidOn;
+end;
+
+
+method __ElementsReadAttribute(aAttributesPtr, aEnd: ^Byte; aTargetParameter: Integer; aTypeFilter: &Class): Attribute; 
+begin
+  var lInst: Attribute;
+  
+  var lTY: __Elements_ProtoReadType;
+  var lKey: Integer;
+  // type & ctor have to be in order.
+  if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) or (lTY <> __Elements_ProtoReadType.length) or (lKey <> 0) then exit nil;
+  var lRealType := objc_getClass(__Elements_ProtoReadString(var aAttributesPtr));
+  if lRealType = nil then exit nil;
+  if (aTypeFilter <> nil) and (aTypeFilter <> lRealType) then exit nil; 
+  if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) or (lTY <> __Elements_ProtoReadType.length) or (lKey <> 1) then exit nil;
+  var lSelector := NSSelectorFromString(__Elements_ProtoReadString(var aAttributesPtr));
+
+  if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) or (lTY <> __Elements_ProtoReadType.varint) or (lKey <> 2) then exit nil;
+  if __Elements_ProtoReadVarInt(var aAttributesPtr) <> aTargetParameter then exit nil;
+
+  lInst := lRealType.alloc;
+  var lSel := lInst.methodSignatureForSelector(lSelector);
+  if lSel = nil then exit nil;
+  var lInvoker := Foundation.NSInvocation.invocationWithMethodSignature(lSel);
+  lInvoker.target := lInst;
+  lInvoker.selector := lSelector;
+  var lArgIdx: Integer := 2;
+  
+  while aAttributesPtr < aEnd do begin 
+    if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) then break;
+    case lKey of
+      3: begin 
+        if lTY <> __Elements_ProtoReadType.startgroup then begin  
+          __Elements_ProtoSkipValue(var aAttributesPtr, lTY);
+        end else begin
+          loop begin
+            var lParName: String;
+            if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) then break;
+            case lKey of 
+              0: begin 
+                lParName := __Elements_ProtoReadString(var aAttributesPtr);
+                if lArgIdx <> 0 then begin 
+                  lInvoker.invoke;
+                  var lNewInst: unretained id;
+                  lInvoker.getReturnValue(@lNewInst);
+                  lInst := lNewInst;
+                  lArgIdx := 0;
+                end;
+              end;
+              1: begin 
+                var lVal: Int64 := __Elements_ProtoReadVarInt(var aAttributesPtr);
+                if lParName = nil then begin
+                  lInvoker.setArgument(@lVal) atIndex(lArgIdx);
+                  inc(lArgIdx)
+                end else 
+                  lInst.setValue(NSNumber.numberWithLongLong(lVal)) forKey(lParName);
+              end;
+              2: begin 
+                var lVal: Double := __Elements_ProtoReadDouble(var aAttributesPtr);
+                if lParName = nil then begin
+                  if lInvoker.methodSignature.getArgumentTypeAtIndex(lArgIdx)^ = 'f' then begin 
+                    // single
+                    var lVal2: Single := lVal;
+                    lInvoker.setArgument(@lVal2) atIndex(lArgIdx);
+                  end else 
+                    lInvoker.setArgument(@lVal) atIndex(lArgIdx);
+                  inc(lArgIdx)
+                end else 
+                  lInst.setValue(NSNumber.numberWithDouble(lVal)) forKey(lParName);
+              end;
+               3: begin 
+                var lVal: String := __Elements_ProtoReadString(var aAttributesPtr);
+                if lParName = nil then begin
+                  lInvoker.setArgument(@lVal) atIndex(lArgIdx);
+                  inc(lArgIdx)
+                end else 
+                  lInst.setValue(lVal) forKey(lParName);
+              end;
+              else __Elements_ProtoSkipValue(var aAttributesPtr, lTY);
+            end;
+          end;
+          continue;
+        end;
+      end;
+    else
+      __Elements_ProtoSkipValue(var aAttributesPtr, lTY);
+    end;
+  end;
+  if lArgIdx <> 0 then begin 
+    lInvoker.invoke;
+    var lNewInst: unretained id;
+    lInvoker.getReturnValue(@lNewInst);
+    lInst := lNewInst;
+  end;
+
+  exit lInst;
+end;
+
+method __ElementsReadAttributes(var aAttributesPtr: ^Byte; aTargetParameter: Integer := 0; aTypeFilter: &Class): NSArray<Attribute>;
+begin
+  if aAttributesPtr = nil then exit nil;
+  var lRes: NSMutableArray<Attribute>;
+  loop begin
+    var lTY: __Elements_ProtoReadType;
+    var lKey: Integer;
+    if not __Elements_ProtoReadHeader(var aAttributesPtr, out lKey, out lTY) or (lKey = 0) then break;
+    case lKey of 
+      1: begin 
+        if lTY <> __Elements_ProtoReadType.message then break; // not a valid message
+        var lLen := __Elements_ProtoReadFixed32(var aAttributesPtr);
+        var lItem := __ElementsReadAttribute(aAttributesPtr, aAttributesPtr + lLen, aTargetParameter, aTypeFilter);
+        aAttributesPtr := aAttributesPtr + lLen;
+        if lItem <> nil then begin
+          if lRes = nil then lRes := new NSMutableArray<Attribute>;
+          lRes.addObject(lItem);
+        end;
+      end;
+    else
+      __Elements_ProtoSkipValue(var aAttributesPtr, lTY);
+    end;
+  end;
+  exit lRes;
+end;
+
+method __ElementsGetAttributesBlockForFunction(aMethod: ^Void): ^Byte;
+begin
+  if aMethod = nil then exit nil;
+  var lWork := ^Byte(aMethod) - 8 - sizeOf(^Byte);
+  if (lWork[0]<> 'E') or (lWork[1] <> 'l') or (lWork[2] <> 'e') or (lWork[3] <> 'm') then exit nil;
+  if (lWork[4]<> 'e') or (lWork[5] <> 'n') or (lWork[6] <> 't') or (lWork[7] <> 's') then exit nil;
+  lWork := lWork + 8;
+  exit (^^Byte(lWork))^;
+end;
+
+method __ElementsGetAttributesBlockForMethod(aClass: &Class; aInstance: Boolean; aSel: SEL): ^Byte;
+begin
+  exit __ElementsGetAttributesBlockForFunction(^Void(if aInstance then 
+    aClass.instanceMethodForSelector(aSel)
+  else
+    aClass.methodForSelector(aSel)
+  ));
 end;
 
 end.
---
+++ Source/ProtoReader.pas
@@ -0,0 +1,102 @@
+﻿namespace RemObjects.Elements.System;
+
+interface
+
+
+type
+  __Elements_ProtoReadType = public (varint = 0, b64 = 1, length = 2, message = 3, b32 = 5, startgroup = 4, endgroup = 6);
+
+method __Elements_ProtoReadHeader(var aSelf: ^Byte; out aKey: Integer; out aType: __Elements_ProtoReadType): Boolean;
+method __Elements_ProtoReadVarInt(var aSelf: ^Byte): Int64;
+method __Elements_ProtoSkipValue(var aSelf: ^Byte; aType: __Elements_ProtoReadType);
+method __Elements_ProtoReadDouble(var aSelf: ^Byte): Double;
+method __Elements_ProtoReadFixed32(var aSelf: ^Byte): Integer;
+method __Elements_ProtoReadFixed64(var aSelf: ^Byte): Int64;
+method __Elements_ProtoReadString(var aSelf: ^Byte): String;
+method __Elements_ProtoReadBytes(var aSelf: ^Byte): array of Byte;
+
+implementation
+
+method __Elements_ProtoReadHeader(var aSelf: ^Byte; out aKey: Integer; out aType: __Elements_ProtoReadType): Boolean;
+begin
+  var lData := __Elements_ProtoReadVarInt(var aSelf);
+  var lType := __Elements_ProtoReadType(lData and $7);
+  aType := lType;
+  aKey := lData shr 3;
+  exit aType <> __Elements_ProtoReadType.endgroup;
+end;
+
+method __Elements_ProtoReadVarInt(var aSelf: ^Byte): Int64;
+begin
+  var lShift := 0;
+  result := 0;
+  loop begin
+    var lData := aSelf^;
+    inc(aSelf);
+    result := result or (Int64(lData and $7f) shl lShift);
+    lShift := lShift + 7;
+    if 0 = (lData and $80) then break;
+  end;
+end;
+
+method __Elements_ProtoSkipValue(var aSelf: ^Byte; aType: __Elements_ProtoReadType);
+begin
+  case aType of
+    __Elements_ProtoReadType.b32: __Elements_ProtoReadFixed32(var aSelf);
+    __Elements_ProtoReadType.b64: __Elements_ProtoReadFixed64(var aSelf);
+    __Elements_ProtoReadType.message: aSelf := aSelf + __Elements_ProtoReadFixed32(var aSelf);
+    __Elements_ProtoReadType.endgroup: ;
+    __Elements_ProtoReadType.startgroup: begin 
+      var lCount := 1;
+      loop begin
+        var lRT := __Elements_ProtoReadType(__Elements_ProtoReadVarInt(var aSelf) and $7);
+        if lRT = __Elements_ProtoReadType.startgroup then 
+          inc(lCount) 
+        else
+        if lRT = __Elements_ProtoReadType.endgroup then begin
+          dec(lCount);
+          if lCount = 0 then break;
+        end else 
+          __Elements_ProtoSkipValue(var aSelf, lRT);
+      end;
+    end;
+    __Elements_ProtoReadType.length: aSelf := aSelf + __Elements_ProtoReadVarInt(var aSelf);
+  else  __Elements_ProtoReadVarInt(var aSelf);
+  end;
+end;
+
+method __Elements_ProtoReadDouble(var aSelf: ^Byte): Double;
+begin
+  result := ^Double(aSelf)^;
+  aSelf := aSelf + 8;
+end;
+
+method __Elements_ProtoReadString(var aSelf: ^Byte): String;
+begin
+  var lLen := __Elements_ProtoReadVarInt(var aSelf);
+  result := new Foundation.NSString withBytes(aSelf) length(lLen) encoding (Foundation.NSStringEncoding.UTF8StringEncoding);
+  aSelf := aSelf + lLen;
+end;
+
+method __Elements_ProtoReadBytes(var aSelf: ^Byte): array of Byte;
+begin
+  var lLen := __Elements_ProtoReadVarInt(var aSelf);
+  result := new Byte[lLen];
+  memcpy(@result[0], aSelf, lLen);
+  aSelf := aSelf + lLen;
+end;
+
+method __Elements_ProtoReadFixed32(var aSelf: ^Byte): Integer;
+begin
+  result := ^Integer(aSelf)^;
+  aSelf := aSelf + 4;
+end;
+
+method __Elements_ProtoReadFixed64(var aSelf: ^Byte): Int64;
+begin
+  result := ^Int64(aSelf)^;
+  aSelf := aSelf + 8;
+end;
+
+
+end.
