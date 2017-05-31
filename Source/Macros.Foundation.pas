20846ad22a6d2875a04c6898586bc3282700edd5
marc hoffman <mh@remobjects.com>
2017-05-31 08:09:54 -0400

    77334: Toffee: HI does not import `NSLocalizedString`

---
+++ Source/Macros.Foundation.pas
@@ -0,0 +1,26 @@
+﻿namespace RemObjects.Elements.System;
+
+uses
+  Foundation;
+
+method NSLocalizedString(aKey: String; aIgnoredComment: String): String; public; inline;
+begin
+  result := NSBundle.mainBundle.localizedStringForKey(aKey) value("") table(nil);
+end;
+
+method NSLocalizedStringFromTable(aKey: String; aTable: String; aIgnoredComment: String): String; public; inline;
+begin
+  result := NSBundle.mainBundle.localizedStringForKey(aKey) value("") table(aTable);
+end;
+
+method NSLocalizedStringFromTableInBundle(aKey: String; aTable: String; aBundle: NSBundle; aIgnoredComment: String): String; public; inline;
+begin
+  result := aBundle.mainBundle.localizedStringForKey(aKey) value("") table(aTable);
+end;
+
+method NSLocalizedStringWithDefaultValue(aKey: String; aTable: String; aBundle: NSBundle; aDefaultValue: String; aIgnoredComment: String): String; public; inline;
+begin
+  result := aBundle.mainBundle.localizedStringForKey(aKey) value(aDefaultValue) table(aTable);
+end;
+
+end.
