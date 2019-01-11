2106dec0a9819f06336c8948ba5504bf3e6c85be
marc hoffman <mh@remobjects.com>
2019-01-11 14:53:37 -0400

    Message property on NSException, for better cross-platform code sharing

--- Source/NSException.pas
+++ Source/NSException.pas
@@ -1,7 +1,5 @@
 ﻿namespace RemObjects.Elements.System;
 
-interface
-
 uses
   Foundation;
 
@@ -10,16 +8,19 @@ type
   private
   protected
   public
+
     constructor(aMessage: String);
+    begin
+      var lClass := self.class.description; // Can't be inlined as self is set to nil during the init call.
+      result := self.initWithName(lClass) reason(aMessage) userInfo(nil);
+    end;
+
+    constructor withError(aError: NSError);
+    begin
+      result := new Exception withName('Exception') reason(aError.description) userInfo(nil);
+    end;
+
     property Message: String read reason;
   end;
-
-implementation
-
-constructor NSException_Toffee(aMessage: String);
-begin
-  var lClass := self.class.description; // Can't be inlined as self is set to nil during the init call.
-  result := self.initWithName(lClass) reason(aMessage) userInfo(nil);
-end;
 
 end.
