ef419c0735e71f196eec9552a64d970fcdb4e984
Elena Pushkariova <elenap@remobjects.com>
2020-06-25 14:02:43 +0300

    Get class info for cocoas types (Island debugger for Toffee)

--- Source/Helpers.pas
+++ Source/Helpers.pas
@@ -13,11 +13,13 @@ method __ElementsClassLockExit(var x: intptr_t); public;
 method __ElementsReadLine(): String; public;
 method __ElementsNullReferenceRaiseException(s: String); public;
 
+method __ElementsObjcClassInfoToString(clz: &Class): String; public;
+
 implementation
 
 method __ElementsNullReferenceRaiseException(s: String);
-begin 
-  if s = nil then 
+begin
+  if s = nil then
     NSException.&raise('NullReferenceException') format('Null Reference Exception');
   NSException.&raise('NullReferenceException') format('Null Reference Exception for expression: %@', s);
 end;
@@ -56,6 +58,155 @@ begin
       exit;
     result := result+Char(c);
   end;
+end;
+
+method __ElementsObjcClassInfoToString(clz: &Class):String;
+begin
+  var cl := class_getName(clz);
+  var s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+  var res := new NSMutableString withCapacity(1024);
+  res.appendString('{"name":"');
+  res.appendString(s);
+  res.appendString('"');
+  var sz := class_getSuperclass(clz);
+  if sz <> nil then begin
+    s := new  Foundation.NSString withCString(class_getName(sz)) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    res.appendString(',"base":"');
+    res.appendString(s);
+    res.appendString('"');
+  end;
+  res.appendString( ',"methods":[');
+  var methodCount: UInt32 := 0;
+  var methods: ^&Method := class_copyMethodList(clz, @methodCount);
+  for i: Integer := 0 to methodCount - 1 do begin
+    var &method: &Method := methods[i];
+    cl := sel_getName(method_getName(&method));
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    if i <> 0 then
+      res.appendString(',');
+    res.appendString('{"selector":"');
+    res.appendString(s);
+    res.appendString('"');
+    cl := method_getTypeEncoding(&method);
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    s := s.stringByReplacingOccurrencesOfString('"') withString('\"');
+    res.appendString(',"signature":"');
+    res.appendString(s);
+    res.appendString('"');
+    var imp := method_getImplementation(&method);
+    var impl := Int64(^IntPtr(@imp)^);
+    res.appendString( ',"implementation":"'+impl);
+    res.appendString('"}');
+    //res.appendString( '}';
+  end;
+  rtl.free(methods);
+  res.appendString(']');
+  begin
+    res.appendString( ',"propeties":[');
+    //var methodCount: UInt32 := 0;
+    var props: ^objc_property_t := class_copyPropertyList(clz, @methodCount);
+    for i: Integer := 0 to methodCount - 1 do begin
+      var &prop := props[i];
+      cl := property_getName(&prop);
+      s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+      if i <> 0 then
+        res.appendString(',');
+      res.appendString( '{"name":"');
+      res.appendString(s);
+      res.appendString('"');
+      cl := property_getAttributes(&prop);
+      s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+      s := s.stringByReplacingOccurrencesOfString('"') withString('\"');
+      res.appendString(',"signature":"');
+      res.appendString(s);
+      res.appendString('"}');
+      //res.appendString( '}';
+    end;
+    rtl.free(props);
+    res.appendString(']');
+  end;
+  begin
+    res.appendString(',"fields":[');
+    //var methodCount: UInt32 := 0;
+    var props: ^Ivar := class_copyIvarList(clz, @methodCount);
+    for i: Integer := 0 to methodCount - 1 do begin
+      var &prop := props[i];
+      cl := ivar_getName(&prop);
+      s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+      if i <> 0 then
+        res.appendString(',');
+      res.appendString( '{"name":"');
+      res.appendString(s);
+      res.appendString('"');
+      cl := ivar_getTypeEncoding(&prop);
+      s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+      s := s.stringByReplacingOccurrencesOfString('"') withString('\"');
+      res.appendString( ',"type":"');
+      res.appendString(s);
+      res.appendString('"');
+      var off := ivar_getOffset(&prop);
+      res.appendString( ',"offset":"'+off);
+      res.appendString('"}');
+      //res.appendString('}';
+    end;
+    rtl.free(props);
+    res.appendString(']');
+  end;
+  clz := objc_getMetaClass(class_getName(clz));
+  res.appendString( ',"classMethods":[');
+  methodCount := 0;
+  methods := class_copyMethodList(clz, @methodCount);
+  for i: Integer := 0 to methodCount - 1 do begin
+    var &method: &Method := methods[i];
+    cl := sel_getName(method_getName(&method));
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    if i <> 0 then
+      res.appendString(',');
+    res.appendString('{"selector":"');
+    res.appendString(s);
+    res.appendString('"');
+    cl := method_getTypeEncoding(&method);
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    s := s.stringByReplacingOccurrencesOfString('"') withString('\"');
+    res.appendString( ',"signature":"');
+    res.appendString(s);
+    res.appendString('"');
+    var imp := method_getImplementation(&method);
+    var impl := Int64(^IntPtr(@imp)^);
+    res.appendString( ',"implementation":"'+impl);
+    res.appendString('"}');
+    //res.appendString( '}';
+  end;
+  rtl.free(methods);
+  res.appendString(']');
+  res.appendString(',"classProperties":[');
+  //var methodCount: UInt32 := 0;
+  var props: ^objc_property_t := class_copyPropertyList(clz, @methodCount);
+  for i: Integer := 0 to methodCount - 1 do begin
+    var &prop := props[i];
+    cl := property_getName(&prop);
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    if i <> 0 then
+      res.appendString(',');
+    res.appendString( '{"name":"');
+    res.appendString(s);
+    res.appendString('"');
+    cl := property_getAttributes(&prop);
+    s := new  Foundation.NSString withCString(cl) encoding(Foundation.NSStringEncoding.UTF8StringEncoding);
+    s := s.stringByReplacingOccurrencesOfString('"') withString('\"');
+    res.appendString(',"signature":"');
+    res.appendString(s);
+    res.appendString('"}');
+    //res.appendString( '}';
+  end;
+  rtl.free(props);
+  res.appendString(']}');
+  // var buf := new AnsiChar[res.length+1];
+  // res.getCString(buf);
+  exit res;
+  // var buf := new AnsiChar[res.length+1];
+  // res.getCString(buf);
+  //exit buf;
 end;
 
 end.
