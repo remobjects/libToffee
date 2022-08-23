d46322e60ebb34b9fe89ec449e3479fc955b4e87
marc hoffman <mh@remobjects.com>
2022-08-23 12:19:24 -0400

    Integer, Index and Range-based indexer properties on NSString

---
+++ Source/NSString.pas
@@ -0,0 +1,26 @@
+﻿namespace RemObjects.Elements.System;
+
+uses
+  Foundation;
+
+type
+  NSString_Extension = public extension class(NSString)
+  public
+
+    property Item[i: Integer]: Char read self.characterAtIndex(i); default;
+    property Item[i: &Index]: Char read self.characterAtIndex(i.GetOffset(length)); default;
+    property Item[r: Range]: String read getSubstringWithRange(r); default;
+
+  private
+
+    method getSubstringWithRange(aRange: Range): NSString;
+    begin
+      var lLength := self.length;
+      var lStart := aRange.fStart.GetOffset(lLength);
+      var lEnd := aRange.fEnd.GetOffset(lLength);
+      result := substringWithRange(NSMakeRange(lStart, lEnd-lStart));
+    end;
+
+  end;
+
+end.
