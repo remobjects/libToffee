cffa0154b1aa952be4bb545e19c88ad929b8a822
marc hoffman <mh@remobjects.com>
2022-09-19 17:01:15 -0400

    E24366: consistent `available()` on all platforms (__ElementsPlatformAndVersionAtLeast); updated for Mac Catalyst 16

--- Source/VersionHelpers.pas
+++ Source/VersionHelpers.pas
@@ -49,48 +49,49 @@ begin
 end;
 
 
-var __ElementsLoadedCocoaVersion: array[0..3] of Integer;
-var __ElementsLoadedUIKitForMacVersion: array[0..3] of Integer;
+var __ElementsPlatformVersion: array[0..3] of Integer;
+var __ElementsUIKitForMacVersion: array[0..3] of Integer;
 
 method __ElementsLoadCocoaVersion;
 begin
-  if __ElementsLoadedCocoaVersion[0] = 1 then
+  if __ElementsPlatformVersion[0] = 1 then
     exit;
 
-  __ElementsLoadedCocoaVersion[0] := 1;
-  __ElementsLoadedUIKitForMacVersion[0] := 1;
+  __ElementsPlatformVersion[0] := 1;
+  __ElementsUIKitForMacVersion[0] := 1;
   if NSProcessInfo.processInfo.respondsToSelector(selector(operatingSystemVersion)) then begin
     {$HIDE NH0}
     var version := NSProcessInfo.processInfo.operatingSystemVersion;
     {$SHOW NH0}
-    __ElementsLoadedCocoaVersion[1] := version.majorVersion;
-    __ElementsLoadedCocoaVersion[2] := version.minorVersion;
-    __ElementsLoadedCocoaVersion[3] := version.patchVersion;
+    __ElementsPlatformVersion[1] := version.majorVersion;
+    __ElementsPlatformVersion[2] := version.minorVersion;
+    __ElementsPlatformVersion[3] := version.patchVersion;
 
     // hack for macOS 11.0 reporting 10.16. Hopefully we can revert before RTM
     if defined("TARGET_OS_MAC") or defined("TARGET_OS_UIKITFORMAC") then begin
-      if (__ElementsLoadedCocoaVersion[1] = 10) and (__ElementsLoadedCocoaVersion[2] = 16) then begin
-        __ElementsLoadedCocoaVersion[1] := 11;
-        __ElementsLoadedCocoaVersion[2] := 0;
+      if (__ElementsPlatformVersion[1] = 10) and (__ElementsPlatformVersion[2] = 16) then begin
+        __ElementsPlatformVersion[1] := 11;
+        __ElementsPlatformVersion[2] := 0;
       end;
     end;
 
     if defined("TARGET_OS_UIKITFORMAC") and CocoaVersionAtLeast(10, 15) then begin
-      if __ElementsLoadedCocoaVersion[1] = 10 then begin
-        if (__ElementsLoadedCocoaVersion[2] in [15, 16]) then begin // Special handling for 10.15 and (temp) 10.16
-          __ElementsLoadedUIKitForMacVersion[1] := __ElementsLoadedCocoaVersion[2]-2; // 15 -> 13, 16 -> 14
-          __ElementsLoadedUIKitForMacVersion[2] := __ElementsLoadedCocoaVersion[3];
-          __ElementsLoadedUIKitForMacVersion[3] := 0;
+      if __ElementsPlatformVersion[1] = 10 then begin
+        if (__ElementsPlatformVersion[2] in [15, 16]) then begin // Special handling for 10.15 and (temp) 10.16
+          __ElementsUIKitForMacVersion[1] := __ElementsPlatformVersion[2]-2; // 15 -> 13, 16 -> 14
+          __ElementsUIKitForMacVersion[2] := __ElementsPlatformVersion[3];
+          __ElementsUIKitForMacVersion[3] := 0;
         end;
       end
       else begin // macOS 11.0 and above
-        __ElementsLoadedUIKitForMacVersion[1] := __ElementsLoadedCocoaVersion[1]+3; // 11 -> 14
-        __ElementsLoadedUIKitForMacVersion[2] := case __ElementsLoadedCocoaVersion[1] of
-          11: __ElementsLoadedCocoaVersion[2]+2; // 11.0 -> 14.2, 11.1 -> 14.3
-          12: if __ElementsLoadedCocoaVersion[2] < 2 then 0 else __ElementsLoadedCocoaVersion[2]-1; // 12.0/12.1 -> 15.0, 12.2 -> 15.1
-          else __ElementsLoadedCocoaVersion[2]; // (guesswork, until we know where macOS 12 goes
+        __ElementsUIKitForMacVersion[1] := __ElementsPlatformVersion[1]+3; // 11 -> 14
+        __ElementsUIKitForMacVersion[2] := case __ElementsPlatformVersion[1] of
+          11: __ElementsPlatformVersion[2]+2; // 11.0 -> 14.2, 11.1 -> 14.3
+        12: if __ElementsPlatformVersion[2] < 2 then 0 else if __ElementsPlatformVersion[2] < 2 then __ElementsPlatformVersion[2]-1 else __ElementsPlatformVersion[2]-2;
+        13: if __ElementsPlatformVersion[2] < 2 then 0 else __ElementsPlatformVersion[2]-1; // 13.0/13.1 -> 16.0, 13.2 -> 16.1?
+        else __ElementsPlatformVersion[2]; // (guesswork, until we know where macOS 14 goes
         end;
-        __ElementsLoadedUIKitForMacVersion[3] := 0;
+        __ElementsUIKitForMacVersion[3] := 0;
       end;
     end;
 
@@ -101,41 +102,41 @@ begin
   // fallback, operatingSystemVersion is new in macOS 10.10, iOS 8,
   //
   {$IFDEF TARGET_OS_IPHONE}
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_8_0 then __ElementsLoadedCocoaVersion := [1, 8, 0, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_7_1 then __ElementsLoadedCocoaVersion := [1, 7, 1, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_7_0 then __ElementsLoadedCocoaVersion := [1, 7, 0, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_6_1 then __ElementsLoadedCocoaVersion := [1, 6, 1, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_6_0 then __ElementsLoadedCocoaVersion := [1, 6, 0, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_5_1 then __ElementsLoadedCocoaVersion := [1, 5, 1, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_5_0 then __ElementsLoadedCocoaVersion := [1, 5, 0, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_3 then __ElementsLoadedCocoaVersion := [1, 4, 3, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_2 then __ElementsLoadedCocoaVersion := [1, 4, 2, 0] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_1 then __ElementsLoadedCocoaVersion := [1, 4, 1, 0] else
-    __ElementsLoadedCocoaVersion := [1, 4, 0, 0];
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_8_0 then __ElementsPlatformVersion := [1, 8, 0, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_7_1 then __ElementsPlatformVersion := [1, 7, 1, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_7_0 then __ElementsPlatformVersion := [1, 7, 0, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_6_1 then __ElementsPlatformVersion := [1, 6, 1, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_6_0 then __ElementsPlatformVersion := [1, 6, 0, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_5_1 then __ElementsPlatformVersion := [1, 5, 1, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_5_0 then __ElementsPlatformVersion := [1, 5, 0, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_3 then __ElementsPlatformVersion := [1, 4, 3, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_2 then __ElementsPlatformVersion := [1, 4, 2, 0] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber_iOS_4_1 then __ElementsPlatformVersion := [1, 4, 1, 0] else
+    __ElementsPlatformVersion := [1, 4, 0, 0];
   exit;
   {$ENDIF}
   {$IFDEF TARGET_OS_MAC and not TARGET_OS_IPHONE}
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_10_1 then __ElementsLoadedCocoaVersion := [1, 10, 10, 1] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_9_2 then __ElementsLoadedCocoaVersion := [1, 10, 9, 2] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_9_1 then __ElementsLoadedCocoaVersion := [1, 10, 9, 1] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_4 then __ElementsLoadedCocoaVersion := [1, 10, 8, 4] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_3 then __ElementsLoadedCocoaVersion := [1, 10, 8, 3] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_2 then __ElementsLoadedCocoaVersion := [1, 10, 8, 2] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_1 then __ElementsLoadedCocoaVersion := [1, 10, 8, 1] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_10_1 then __ElementsPlatformVersion := [1, 10, 10, 1] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_9_2 then __ElementsPlatformVersion := [1, 10, 9, 2] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_9_1 then __ElementsPlatformVersion := [1, 10, 9, 1] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_4 then __ElementsPlatformVersion := [1, 10, 8, 4] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_3 then __ElementsPlatformVersion := [1, 10, 8, 3] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_2 then __ElementsPlatformVersion := [1, 10, 8, 2] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_8_1 then __ElementsPlatformVersion := [1, 10, 8, 1] else
 
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_4 then __ElementsLoadedCocoaVersion := [1, 10, 7, 4] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_3 then __ElementsLoadedCocoaVersion := [1, 10, 7, 3] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_2 then __ElementsLoadedCocoaVersion := [1, 10, 7, 2] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_1 then __ElementsLoadedCocoaVersion := [1, 10, 7, 1] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_4 then __ElementsPlatformVersion := [1, 10, 7, 4] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_3 then __ElementsPlatformVersion := [1, 10, 7, 3] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_2 then __ElementsPlatformVersion := [1, 10, 7, 2] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_7_1 then __ElementsPlatformVersion := [1, 10, 7, 1] else
 
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_8 then __ElementsLoadedCocoaVersion := [1, 10, 6, 8] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_6 then __ElementsLoadedCocoaVersion := [1, 10, 6, 6] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_5 then __ElementsLoadedCocoaVersion := [1, 10, 6, 5] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_4 then __ElementsLoadedCocoaVersion := [1, 10, 6, 4] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_3 then __ElementsLoadedCocoaVersion := [1, 10, 6, 3] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_2 then __ElementsLoadedCocoaVersion := [1, 10, 6, 2] else
-  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_1 then __ElementsLoadedCocoaVersion := [1, 10, 6, 1] else
-    __ElementsLoadedCocoaVersion := [1, 10, 5, 0];
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_8 then __ElementsPlatformVersion := [1, 10, 6, 8] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_6 then __ElementsPlatformVersion := [1, 10, 6, 6] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_5 then __ElementsPlatformVersion := [1, 10, 6, 5] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_4 then __ElementsPlatformVersion := [1, 10, 6, 4] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_3 then __ElementsPlatformVersion := [1, 10, 6, 3] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_2 then __ElementsPlatformVersion := [1, 10, 6, 2] else
+  if NSFoundationVersionNumber >=  NSFoundationVersionNumber10_6_1 then __ElementsPlatformVersion := [1, 10, 6, 1] else
+    __ElementsPlatformVersion := [1, 10, 5, 0];
   exit;
   {$ENDIF}
 end;
@@ -143,11 +144,11 @@ end;
 method __ElementsCocoaVersionAtLeast(aMaj, aMin: Integer; aRev: Integer := 0): Boolean;
 begin
   __ElementsLoadCocoaVersion;
-  if (aMaj > __ElementsLoadedCocoaVersion[1]) then exit false;
-  if (aMaj = __ElementsLoadedCocoaVersion[1]) then begin
-    if (aMin > __ElementsLoadedCocoaVersion[2]) then exit false;
-    if (aMin = __ElementsLoadedCocoaVersion[2]) then begin
-      if (aRev > __ElementsLoadedCocoaVersion[3]) then exit false;
+  if (aMaj > __ElementsPlatformVersion[1]) then exit false;
+  if (aMaj = __ElementsPlatformVersion[1]) then begin
+    if (aMin > __ElementsPlatformVersion[2]) then exit false;
+    if (aMin = __ElementsPlatformVersion[2]) then begin
+      if (aRev > __ElementsPlatformVersion[3]) then exit false;
     end;
   end;
   exit true;
@@ -156,11 +157,11 @@ end;
 method __ElementsUIKitForMacVersionAtLeast(aMaj, aMin: Integer; aRev: Integer := 0): Boolean;
 begin
   __ElementsLoadCocoaVersion;
-  if (aMaj > __ElementsLoadedUIKitForMacVersion[1]) then exit false;
-  if (aMaj = __ElementsLoadedUIKitForMacVersion[1]) then begin
-    if (aMin > __ElementsLoadedUIKitForMacVersion[2]) then exit false;
-    if (aMin = __ElementsLoadedUIKitForMacVersion[2]) then begin
-      if (aRev > __ElementsLoadedUIKitForMacVersion[3]) then exit false;
+  if (aMaj > __ElementsUIKitForMacVersion[1]) then exit false;
+  if (aMaj = __ElementsUIKitForMacVersion[1]) then begin
+    if (aMin > __ElementsUIKitForMacVersion[2]) then exit false;
+    if (aMin = __ElementsUIKitForMacVersion[2]) then begin
+      if (aRev > __ElementsUIKitForMacVersion[3]) then exit false;
     end;
   end;
   exit true;
@@ -185,21 +186,21 @@ end;
 method __ElementsCocoaVersionString: String;
 begin
   __ElementsLoadCocoaVersion;
-  exit NSString.stringWithFormat('%d.%d.%d', __ElementsLoadedCocoaVersion[1],__ElementsLoadedCocoaVersion[2],__ElementsLoadedCocoaVersion[3]);
+  exit NSString.stringWithFormat('%d.%d.%d', __ElementsPlatformVersion[1],__ElementsPlatformVersion[2],__ElementsPlatformVersion[3]);
 end;
 
 method __ElementsCocoaUIKitForMacVersionString: String;
 begin
   __ElementsLoadCocoaVersion;
-  exit NSString.stringWithFormat('%d.%d.%d', __ElementsLoadedUIKitForMacVersion[1],__ElementsLoadedUIKitForMacVersion[2],__ElementsLoadedUIKitForMacVersion[3]);
+  exit NSString.stringWithFormat('%d.%d.%d', __ElementsUIKitForMacVersion[1],__ElementsUIKitForMacVersion[2],__ElementsUIKitForMacVersion[3]);
 end;
 
 method __ElementsCocoaVersion: array[0..2] of Integer;
 begin
   __ElementsLoadCocoaVersion;
-  result[0] := __ElementsLoadedCocoaVersion[1];
-  result[1] := __ElementsLoadedCocoaVersion[2];
-  result[2] := __ElementsLoadedCocoaVersion[3];
+  result[0] := __ElementsPlatformVersion[1];
+  result[1] := __ElementsPlatformVersion[2];
+  result[2] := __ElementsPlatformVersion[3];
 end;
 
 method __ElementsCocoaPlatform: String;
